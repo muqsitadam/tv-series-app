e94f04eff4739269b513b30bb6f80fb6e8cb659c
Abdulmuqsit Adam <muqsitadam@users.noreply.github.com>
2020-08-17 20:35:54 +0100

    Moved and also deleted some files

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
 import Intro from "../Intro";
+import Series from "../../containers/Series";
 import "./App.css";
 import "whatwg-fetch";
 
 class App extends Component {
-  state = {
-    series: [],
-  };
-
-  // componentDidMount() {
-  //   const series = ["Vikings", "The Witcher"];
-
-  //   setTimeout(() => {
-  //     this.setState({ series });
-  //     //If they have the same name like that series...
-  //     //You can use just one of them.
-  //   }, 2000);
-  // }
-
-  componentDidMount() {
-    fetch("http://api.tvmaze.com/search/shows?q=Vikings")
-      .then((response) => response.json())
-      .then((json) => this.setState({ series: json }));
-  }
   render() {
     return (
       <div className="App">
@@ -30,7 +12,7 @@ class App extends Component {
           <p>TV Series List</p>
         </header>
         <Intro message="Here you can find all of your most watched series" />
-        The number of series here is : {this.state.series.length};
+        <Series />
       </div>
     );
   }
