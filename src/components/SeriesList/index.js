35ae3edf9d10151a3ca39f34968f094cb715fa8a
Abdulmuqsit Adam <muqsitadam@users.noreply.github.com>
2020-08-19 17:18:02 +0100

    Created the list of the TV series

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SeriesList = (props) => {
-  return <div>Series List Component</div>;
+  return (
+    <div>
+      <ul>
+        {props.list.map((series) => (
+          <li>{series.show.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SeriesList;
