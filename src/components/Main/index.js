73c98fdeb83c0ec2009929d08f4d7fd5fb5c9dac
Abdulmuqsit Adam <muqsitadam@users.noreply.github.com>
2020-08-20 13:33:48 +0100

    Set up routes and use the routes params

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Series from "../../containers/Series";
+import SingleSeries from "../../containers/SingleSeries";
 
 const Main = (props) => {
   return (
     <Switch>
       <Route exact path="/" component={Series} />
+      <Route path="/series/:id" component={SingleSeries} />
     </Switch>
   );
 };
