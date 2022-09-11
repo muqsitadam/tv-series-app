89c4a41af27adee376b05427fa14fd450831dc0c
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-09-11 15:52:22 +0100

    Components folder

---
+++ src/Components/SearchBar/SearchInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchInput = () => {
+    return(
+        <div>
+            <h2>Search for your favourite series!!</h2>
+            <div>
+                <input type="search" name="" id="" />
+                <button>Search</button>
+            </div>
+        </div>
+    )
+}
+
+export default SearchInput
