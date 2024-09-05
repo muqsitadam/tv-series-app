86b77752c2467184c59200bb0bbed262264c2a8a
muqsitadam <muqsitadam@gmail.com>
2024-09-05 15:29:53 +0100

    Added my tv logo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
+import logo from "../../Assets/logo.png"
 
 function Navbar() {
     return(
-        <nav className="flex flex-row w-full bg-slate-700 py-12 text-zinc-50">
-            <a href="/" className="w-2/3 mx-1 text-lg">Logo</a>
-            <ul className="flex flex-row flex-grow">
-                <li className="basis-1/3 cursor-pointer hover:text-lg">
+        <nav className="flex flex-row w-full bg-slate-700 text-zinc-50">
+            <a href="/" className="w-2/3">
+                <img
+                    src={logo}
+                    className="w-16 h-16"
+                />
+            </a>
+            <ul className="flex flex-row flex-grow ">
+                <li className="basis-1/3 mt-4 cursor-pointer hover:text-lg">
                     <a href="/crime">Crime</a>
                 </li>
-                <li className="basis-1/3 cursor-pointer hover:text-lg">
+                <li className="basis-1/3 mt-4 cursor-pointer hover:text-lg">
                     <a href="/drama">Drama</a>
                 </li>
-                <li className="basis-1/3 cursor-pointer hover:text-lg">
+                <li className="basis-1/3 mt-4 cursor-pointer hover:text-lg">
                     <a href="/action">Action</a>
                 </li>
             </ul>            
