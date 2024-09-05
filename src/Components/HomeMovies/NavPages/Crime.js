885b8881425191f53ab10788e5798e8a0ac27e94
muqsitadam <muqsitadam@gmail.com>
2024-09-05 14:50:23 +0100

    Added content to the links on the navbar

---
+++ src/Components/HomeMovies/NavPages/Crime.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+function Crime (){
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.tvmaze.com/shows')
+      .then((response) => response.json())
+      .then((data) => {
+              const filteredShows = data.filter((show) => show.genres.includes("Crime"));
+              const reducedShows = filteredShows.slice(0,20)
+              setShows(reducedShows)})
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+  console.log(shows)
+
+    return(
+      <div>
+        <p className="mt-4 text-2xl text-center font-mono italic font-bold">Crime Series</p>
+        <hr/>
+        <ul className="flex flex-row flex-grow flex-wrap justify-center">
+          {shows.map((series) => {
+            const imageUrl = series.image ? series.image.medium : null;
+
+            return(
+              <div key={series.id} className="m-2 w-60 border-2 border-bluegray-300 rounded-xl">
+            {imageUrl && (
+              <img
+                src={imageUrl}
+                alt={series.name}
+                className="w-full rounded-t-xl mb-1" 
+              />
+            )}
+             <p className="px-1 my-1"><span className="italic font-medium font-lg"> Title:</span> {series.name}</p>
+             <p className="px-1 my-1"><span className="italic font-medium font-lg"> Release Date:</span> {series.premiered}</p>
+             <p className="px-1 my-1 text-blue-800"><a target="blank" href={series.url} className="font-medium font-lg"> Read more...</a></p>
+             </div>
+            )
+          })}
+        </ul>
+      </div>
+    )
+}
+
+export default Crime
